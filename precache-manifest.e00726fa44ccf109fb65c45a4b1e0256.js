2c0e98a114af821c2c020cbcd8d6451b3cd27531
Kanikashrivastav <kanika17@navgurukul.org>
2019-01-05 20:12:54 +0530

    Updates

---
+++ precache-manifest.e00726fa44ccf109fb65c45a4b1e0256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bd92c94f636b5073f1f",
+    "url": "/missingChildrenApp/static/css/main.f3da60e6.chunk.css"
+  },
+  {
+    "revision": "8bd92c94f636b5073f1f",
+    "url": "/missingChildrenApp/static/js/main.8bd92c94.chunk.js"
+  },
+  {
+    "revision": "69dae8ba88c28691a512",
+    "url": "/missingChildrenApp/static/js/1.69dae8ba.chunk.js"
+  },
+  {
+    "revision": "1cb5142f41f46ba395db",
+    "url": "/missingChildrenApp/static/js/runtime~main.1cb5142f.js"
+  },
+  {
+    "revision": "441d69764a6c41470f441e3c6973ac2c",
+    "url": "/missingChildrenApp/index.html"
+  }
+];
