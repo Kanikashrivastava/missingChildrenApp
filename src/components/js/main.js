65658a428f3a383aea301c6f6e8cc8af2e94025c
Kanikashrivastava <kanikashrivastava1999@gmail.com>
2019-04-02 13:40:56 +0530

    footer part is left in the repo have to work on it

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import "/home/kanika/Desktop/missingchild/src/App.css";
+// import "/home/kanika/Desktop/missingchild/src/App.css";
 import Slider from "./slide"
 class MainPage extends Component {
   render() {
     return (
       <div className="App">
         <Slider/>
-
+        {/* have to make footer here for the repo here! */}
+        {/* <p>footer</p> */}
       </div>
     );
   }
